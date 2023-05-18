cd33452d43d8d08ae70754d2a7bd292b6c354778
Yiqing Liang <yiqing.liang@hotmail.com>
2023-05-18 16:25:35 +0300

    init

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,7 @@
+class AssetManager {
+  static String imagePath = "assets/images";
+  static String userImage = "$imagePath/person.png";
+  static String openaiLogo = "$imagePath/openai_logo.jpg";
+  static String botImage = "$imagePath/chat_logo.png";
+
+}
